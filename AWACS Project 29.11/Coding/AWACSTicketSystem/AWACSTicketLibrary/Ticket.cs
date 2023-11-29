8cea16070833b1baf43f7aabda8f477639c6d88f
Pablog223 <pag3988@thi.de>
2023-11-29 14:52:26 +0100

    AWACS Ticket Application first upload

---
+++ AWACS Project 29.11/Coding/AWACSTicketSystem/AWACSTicketLibrary/Ticket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketLibrary
+{
+    public class Ticket
+    {
+        private User _responsible;
+        public User Responsible
+        {
+            get { return _responsible; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Every ticket must have a responsible");
+                _responsible = value;
+            }
+        }
+                    
+        private User _creator;
+        public User Creator
+        {
+            get { return _creator; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Every ticket must have a creator");
+                _creator = value;
+            }
+        }
+        private string _id;
+        public string ID 
+        { 
+            get {  return _id; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("A valid ticket ID number is required");
+                _id = value;
+            }               
+        }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Solution { get; set; }
+        public string DateObserved { get; set; }
+        public string PlannedEndDate { get; set; }
+        public string Status { get; set; }
+        public string Progress { get; set; }
+    }
+}
+
---
+++ AWACS Project 29.11/Coding/AWACSTicketSystem/AWACSTicketLibrary/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketLibrary
+{
+    public class User
+    {
+        public int WorkerID { get; set; }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Invalid name");
+                _name = value;
+            }  
+        }
+        
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+
+    }
+}
