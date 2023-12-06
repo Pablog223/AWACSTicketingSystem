3955c9d5671a7798658258469dde3fed2ae2e6fc
Pablog223 <pag3988@thi.de>
2023-12-06 20:37:05 +0100

    File upload on the 06.12

---
+++ AWACS Project 06.12/AWACSTicketSystem/AWACSTicketLibrary/Ticket.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketLibrary
+{
+    public class Ticket
+    {
+        private string _ticketID;
+        public string TicketID
+        {
+            get { return _ticketID; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("A valid ticket ID number is required");
+                _ticketID = value;
+            }
+        }
+        private string _category;
+        public string Category
+        {
+            get { return _category; }
+            set
+            {
+                if (value == null) throw new Exception("A category for this ticket is required");
+                _category = value;
+            }
+        }
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value == null) throw new Exception("A description for this ticket is required");
+                _description = value;
+            }
+        }
+
+//simple properties
+
+        public string Solution { get; set; }
+        public string DateObserved { get; set; }
+        public string PlannedEndDate { get; set; }
+        public string Status { get; set; }
+        public string Progress { get; set; }
+
+//the constructor for the class ticket is built
+
+        public Ticket(string TicketID, string Category, string Description)
+        {
+            _category = Category;
+            _ticketID = TicketID;
+            _description = Description;
+        }
+
+//converting the code into methods
+
+        
+           
+       
+    }
+
+}
+
---
+++ AWACS Project 06.12/AWACSTicketSystem/AWACSTicketSystemLibrary/Extension/User.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketSystemLibrary
+{
+    public partial class User
+
+//attributes with validation which we will use in the constructor further on
+
+    {
+        private string _workerID;
+        public string WorkerID
+        {
+            get { return _workerID; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("A worker ID is required");
+                _workerID = value;
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Invalid name");
+                _name = value;
+            }  
+        }
+        private string _surname;
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Invalid name");
+                _surname = value;
+            }
+        }
+
+        private List<Ticket> _created = new List<Ticket>();
+        public List<Ticket> Created
+        {
+            get { return _created; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Every ticket must have a creator");
+                _created = value;
+            }
+        }
+
+        private List<Ticket> _responsible = new List<Ticket>();
+        public List<Ticket> Responsible
+        {
+            get { return _responsible; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Every ticket must have at least one responsible");
+                _responsible = value;
+            }
+        }
+
+//two simple properties inside the class
+
+        public string Department { get; set; }
+        public string Email { get; set; }
+
+//a constructor is used to stablish the parameters we want whenever a new object is created
+//I guess, that is the reason why we make it private, because it will be accessed through the constructor 
+
+        public User(string WorkerID, string Name, string Surname)
+        {
+            _workerID = WorkerID;
+            _name = Name;
+            _surname = Surname;
+        }
+    }
+}
