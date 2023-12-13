eee0f9a3106b806c19716d265144fadcf20e04d0
Pablog223 <pag3988@thi.de>
2023-12-13 21:35:27 +0100

    AWACS 13.12

---
+++ AWACS Project 12.12/Coding/AWACSTicketSystem/AWACSTicketLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketLibrary
+{
+    public class Class1
+    {
+    }
+}
