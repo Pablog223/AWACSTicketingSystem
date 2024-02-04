85c603280e26e844e96430784b4d4c073adff609
Pablog223 <pag3988@thi.de>
2024-02-04 13:16:47 +0100

    AWACS Ticket System Final Version 04.02.24

---
+++ AWACS Final 04.02.24/AWACSTicketSystem/AWACSTicketLibrary/Strings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWACSTicketLibrary
+{
+    public partial class Worker
+    {
+        public override string ToString()
+        {
+            return $"{Name} {Surname} with the Worker Identification {WorkerId}";
+        }
+        public string AsString => ToString();
+    }
+    public partial class Ticket
+    {
+        public override string ToString()
+        {
+            return $"This is the Ticket {TicketId} created by {Creator} and with the responsible {Responsible}";
+        }
+        public string AsString => ToString();
+    }
+
+}
---
+++ AWACS Final 04.02.24/AWACSTicketSystem/AWACSTicketWeb/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using AWACSTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AWACSTicketWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private AWACSModelContainer db = new AWACSModelContainer();
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["idUser"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        //GET: Register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "Id,WorkerId,Name,Surname,eMail,Password,Department")] Worker _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.WorkerSet.FirstOrDefault(s => s.eMail == _user.eMail);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.WorkerSet.Add(_user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Tickets");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = db.WorkerSet.Where(s => s.eMail.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().Name + " " + data.FirstOrDefault().Surname;
+                    Session["Email"] = data.FirstOrDefault().eMail;
+                    Session["idUser"] = data.FirstOrDefault().Id;
+                    return RedirectToAction("Index", "Tickets");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return View();
+                }
+            }
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+    }
+}
